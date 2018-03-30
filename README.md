f505413729e5757f09f24948b20f9a7cbffe383a
Barry Williams <barrywilliams84@gmail.com>
2018-03-30 01:18:59 -0700

    app built and working. Scrapes packers.com for all news content including articles, videos and podcasts. Saves items to mongoDB via mongoose schema and methods. Still quite a few bugs to work out

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# mongoScraper
+# Packers News mongoScraper 
+A web-scraping app developed using mongo DB, mongoose and npm cherio package. 
+
+## Description / Features
+## Dependencies
+## Framework used
+## Demo
+	<video.mov>
+## Author
+Barry Williams
+@makeitsoo
--- server.js
+++ server.js
@@ -1,23 +1,4 @@
 
-// demo application [here](http://nyt-mongo-scraper.herokuapp.com/)
-
-// * Create an app that accomplishes the following:
-
-//   1. Whenever a user visits your site, the app should scrape stories from a news outlet of your choice and display them for the user. Each scraped article should be saved to your application database. At a minimum, the app should scrape and display the following information for each article:
-
-//      * Headline - the title of the article
-
-//      * Summary - a short summary of the article
-
-//      * URL - the url to the original article
-
-//      * Feel free to add more content to your database (photos, bylines, and so on).
-
-//   2. Users should also be able to leave comments on the articles displayed and revisit them later. The comments should be saved to the database as well and associated with their articles. Users should also be able to delete comments left on articles. All stored comments should be visible to every user.
-
-
-//---------------------------------------------------------------------------------------------------------------
-
 // required files and packages
 var express = require("express");
 var exphbs = require("express-handlebars");
@@ -53,18 +34,6 @@ if(process.env.MONGODB_URI) {
     mongoose.connect(dbConnect);
 }
 
-// Connect mongoose to our database
-	// mongodb://foxsScrape:password12@ds119585.mlab.com:19585/heroku_hd8909ql;
-	/* mongoose.connect(dbConnect, function (error) {
-    // Log any errors connecting with mongoose
-    if (error) {
-        console.log(error);
-    }
-    // Or log a success message
-    else {
-        console.log("Mongoose connection is successful");
-    }
-	}); */
 
 // mongoose to db connection 
 var db = mongoose.connection;
